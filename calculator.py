2c6a885f3fbbe00ec34e0419f6bceb8dcdac6e16
vibhusehra <vibhusehra@gmail.com>
2019-05-31 16:21:30 +0530

    live predictions working.

--- calculator.py
+++ calculator.py
@@ -13,11 +13,12 @@ h = 200
 model = load_model('model.h5')
 
 def predictionImage(roi,thresh):
-	img = np.zeros_like(roi)
+	img = np.zeros_like(roi,np.float32)
 	img[:,:,0] = thresh
 	img[:,:,1] = thresh
 	img[:,:,2] = thresh
 	img = img.reshape(1,200,200,3)
+	img /= 255.
 
 	return img
 
@@ -28,7 +29,7 @@ def imagePreprocess(frame):
 
 	hsv = cv2.cvtColor(roi,cv2.COLOR_BGR2HSV)
 	#mask for thresholding the skin color
-	mask = cv2.inRange(hsv,np.array([2,30,50]),np.array([30,255,255]))
+	mask = cv2.inRange(hsv,np.array([2,20,50]),np.array([30,255,255]))
 
 	#reducing the noise in the image
 	kernel = np.ones((5,5))
@@ -42,11 +43,24 @@ def imagePreprocess(frame):
 	
 
 	return mask,thresh,img
+
+def writeTextToWindow(img,text,default_x_calc,default_y_calc):
+	fontscale = 1.0
+	color = (0, 0, 0)
+	fontface = cv2.FONT_HERSHEY_COMPLEX_SMALL
+	cv2.putText(img, str(text), (default_x_calc, default_y_calc), fontface, fontscale, color)
 	
 
+	return img
 
+predArray = [-1,-1]
+default_y_calc = 80	
+default_x_calc = 25
 predCount = 0 #for confirming the number displayed
 predPrev = 0
+result = np.zeros((300,300,3),np.uint8)
+result.fill(255) #fill result window(make it white)
+cv2.putText(result,"Calculator", (25, 40), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.0, (0,0,0))
 while True:
 	ret,frame = cap.read()
 	
@@ -57,7 +71,24 @@ while True:
 
 	if predCount > 15:
 		print('Prediction: ' + str(predPrev))
-		#putTextInResult(blackboard, predPrev)
+		if predArray[0] == -1:
+			predArray[0] = predPrev
+			string = '{} + '.format(predArray[0])
+			writeTextToWindow(result,string,default_x_calc,default_y_calc)
+			default_x_calc += 20
+
+		else:
+			default_x_calc += 40
+			predArray[1] = predPrev
+			string = '{} = {}'.format(predArray[1],np.sum(predArray,axis=0))
+			writeTextToWindow(result,string,default_x_calc,default_y_calc)
+
+			default_x_calc = 25
+			default_y_calc += 30
+			#writeTextToWindow(result,' = ',default_x_calc,default_y_calc)
+			#writeTextToWindow(result,,default_x_calc,default_y_calc)
+			print("Sum: {}".format(np.sum(predArray,axis=0)))
+			predArray = [-1,-1]
 		predCount = 0
 
 	
@@ -71,11 +102,9 @@ while True:
 	predPrev = pred
 	
 	#print(pred)
-
-
-	#result = np.zeros((200, 200, 3), dtype=np.uint8)
-	#res = np.hstack((result, blackboard))
-	#cv2.imshow("Recognizing gesture", res)
+	#result = writeTextToWindow(result,predPrev)
+	
+	cv2.imshow("result",result)
 	cv2.imshow('frame',frame)
 	cv2.imshow('roi',mask)
 	cv2.imshow('thresh',thresh)
